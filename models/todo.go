f1719a81f55ccc4741de9d79ef4d0b1311b1d7bd
chmuhammadasim <boybrave82@gmail.com>
2024-09-04 22:59:20 +0500

    feat: Add created_at and updated_at fields to Todo model

--- models/todo.go
+++ models/todo.go
@@ -1,9 +1,15 @@
 package models
 
-import "go.mongodb.org/mongo-driver/bson/primitive"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type Todo struct {
 	ID        primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
 	Title     string             `json:"title,omitempty" bson:"title,omitempty"`
 	Completed bool               `json:"completed,omitempty" bson:"completed,omitempty"`
+	CreatedAt time.Time          `json:"created_at,omitempty" bson:"created_at,omitempty"`
+	UpdatedAt time.Time          `json:"updated_at,omitempty" bson:"updated_at,omitempty"`
 }
